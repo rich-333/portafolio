2e51a58ad5cefaf0fb8ecb493a1be343444a7c1a
rich-333 <interriky@gmail.com>
2026-05-15 09:45:27 -0400

    Agregar componentes de Header y Footer: implementar navegación y datos de pie de página

---
+++ src/data/ui/footer.ts
@@ -0,0 +1,15 @@
+export interface FooterData {
+  name: string;
+  href: string;
+}
+
+export const footerData: FooterData[] = [
+  {
+    name: "GITHUB",
+    href: "https://github.com"
+  },
+  {
+    name: "LINKEDIN",
+    href: "https://linkedin.com"
+  }
+];
---
+++ src/data/ui/header.ts
@@ -0,0 +1,31 @@
+export interface Navlinks {
+  name: string;
+  href: string;
+}
+
+export const navLinks: Navlinks[] = [
+  {
+    name: 'Inicio',
+    href: '/',
+  },
+  {
+    name: 'Sobre mi',
+    href: '#about',
+  }, 
+  {
+    name: 'Proyectos',
+    href: '#projects',
+  },
+  {
+    name: 'Tecnologías',
+    href: '#technologies',
+  },
+  {
+    name: 'Certificados',
+    href: '#certificates',
+  },
+  {
+    name: 'Contacto',
+    href: '#contact',
+  }
+]
