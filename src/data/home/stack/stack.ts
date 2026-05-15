dd06b52500a7e5215bc6869157ea3cc017c459a4
rich-333 <interriky@gmail.com>
2026-05-15 09:47:32 -0400

    Agregar componente Stack: implementar sección de tecnologías con datos y estilos

---
+++ src/data/home/stack/stack.ts
@@ -0,0 +1,68 @@
+export interface Stack {
+  id: number;
+  name: string;
+  icon: string;
+}
+
+export const stack: Stack[] = [
+  {
+    id: 1,
+    name: "Laravel",
+    icon: "material-icon-theme:laravel"
+  },
+  {
+    id: 2,
+    name: "React",
+    icon: "devicon:react"
+  },
+  {
+    id: 3,
+    name: "Astro",
+    icon: "vscode-icons:file-type-astro"
+  },
+  {
+    id: 4,
+    name: "Tailwind CSS",
+    icon: "devicon:tailwindcss"
+  },
+  {
+    id: 5,
+    name: "Git",
+    icon: "devicon:git"
+  },
+  {
+    id: 6,
+    name: "GitHub",
+    icon: "devicon:github"
+  },
+  {
+    id: 7,
+    name: "PostgreSQL",
+    icon: "devicon:postgresql"
+  },
+  {
+    id: 8,
+    name: "MySQL",
+    icon: "devicon:mysql"
+  },
+  {
+    id: 9,
+    name: "JavaScript",
+    icon: "devicon:javascript"
+  },
+  {
+    id: 10,
+    name: "Typescript",
+    icon: "devicon:typescript"
+  },
+  {
+    id: 11,
+    name: "C#",
+    icon: "logos:c-sharp"
+  },
+  {
+    id: 12,
+    name: "PHP",
+    icon: "devicon:php"
+  }
+]
